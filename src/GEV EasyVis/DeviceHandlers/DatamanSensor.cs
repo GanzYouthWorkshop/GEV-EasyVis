0988bef9622b73277bf0870c2fca9b00a9ba12b9
Unknown <kimmelgabor@gmail.com>
2018-05-29 16:40:11 +0200

    Dataman device development start

--- src/GEV EasyVis/DeviceHandlers/DatamanSensor.cs
+++ src/GEV EasyVis/DeviceHandlers/DatamanSensor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cognex.DataMan.SDK;
+using Cognex.DataMan.SDK.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +17,38 @@ namespace GEV.EasyVis.DeviceHandlers
         public string Password { get; set; }
         public string Username { get; set; }
 
+        public DataManSystem Sensor { get; private set; }
+        public ResultCollector Collector { get; private set; }
+
         public void Connect()
+        {
+            EthSystemConnector connection = new EthSystemConnector(System.Net.IPAddress.Parse(this.IPAddress))
+            {
+                UserName = "admin",
+                Password = "",
+            };
+
+            this.Sensor = new DataManSystem(connection);
+            this.Sensor.DefaultTimeout = 5000;
+            this.Sensor.SetKeepAliveOptions(true, 3000, 1000);
+
+            ResultTypes requestedResults = ResultTypes.ReadXml | ResultTypes.Image | ResultTypes.ImageGraphics;
+
+            this.Collector = new ResultCollector(this.Sensor, requestedResults);
+            this.Collector.ComplexResultArrived += M_DatamanResultCollector_ComplexResultArrived;
+
+            this.Sensor.Connect();
+
+            try
+            {
+                this.Sensor.SetResultTypes(requestedResults);
+            }
+            catch
+            {
+            }
+        }
+
+        private void M_DatamanResultCollector_ComplexResultArrived(object sender, ResultInfo e)
         {
             throw new NotImplementedException();
         }
